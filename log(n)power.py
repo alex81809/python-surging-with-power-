470920d51c4bbfadc0703b3b341780a0793f1409
Alexandra <asjlobaton@gmail.com>
2026-02-11 19:08:43 +0300

    Create log(n)power.py

---
+++ log(n)power.py
@@ -0,0 +1,23 @@
+# program to compute x ^ y without using math function
+
+
+def computePower(x, y):
+
+    # default total is 1 
+    result = 1
+
+    while (y > 0):
+        # if y is even 
+        if (y % 2 == 0):
+            x = x * x
+            y >>= 1
+        
+        else: 
+            result = result * x
+            y = y - 1
+
+        return result
+
+x = int(input("Enter x for x^y: "))
+y = int(input("Enter y for x^y: "))
+print("Total: ",(computePower(x, y)))
